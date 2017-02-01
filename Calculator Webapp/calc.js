0e3c531a8d2c456dac009033a0829284af968757
jswz72 <jacobdasword@gmail.com>
2017-01-31 23:46:08 -0500

    Fix attempt
    Attempted to fix multi-operator functions. Cleaned up style. Still broken.

--- Calculator Webapp/calc.js
+++ Calculator Webapp/calc.js
@@ -1,80 +1,72 @@
-$(document).ready(function(){
- // console.log("ready");
+$(document).ready(function() {
+  // console.log("ready");
   var afterOp = false;
   //console.log("button success");
-  var expression;   //collect button input
+  var expression; //collect button input
   var a;
   var operatorVal;
-  var operatorArr = ['/','*','-','+']
-  var opScan = true;
+  var operatorArr = ['/', '*', '-', '+']
+  var opScan = false;
   var b;
-  var answer = false;
-  
-  $("button").click(function(){
-    
-    var btnVal = $(this).attr('value');   //html value of button pressed
-    // console.log("a");
-    // console.log(btnVal);
-    
-    if(answer == true){
+  var answer = false; //Flag: True if answer to calculation displayed on screen
+
+  $("button").click(function() {
+
+    var btnVal = $(this).attr('value'); //html value of button pressed
+
+    //After answer displayed, clear out window before continuing
+    if (answer) {
       $("#history").html('');
       $("#input").html('');
       answer = false;
     }
-    
+
     //if equals button was pushed, move input--> history and calculate
-    if(btnVal === '='){
-      console.log("b");
+    if (btnVal === '=') {
+      //console.log("b");
       b = $("#input").html();
       $("#history").html(a + operatorVal + b);
       console.log(a + operatorVal + b);
       $("#input").html(math.eval(a + operatorVal + b));
       answer = true;
-     
+      opScan = true;
+
     }
     //if operator pressed, make sure no other operator stored until cleared by '='
-    else if(btnVal === '/' || btnVal === '*' || btnVal === '-' || btnVal === '+'){
-      // console.log("c");
-      // console.log($("#input").html().indexOf(btnVal));
-      for(var i = 0; i < operatorArr.length; i++){
-        if($("#input").html().indexOf(operatorArr[i]) != -1){
-          opScan = false;
-        }
-      }
-      
+    else if (btnVal === '/' || btnVal === '*' || btnVal === '-' || btnVal === '+') {
       if(opScan){
-       console.log("d");
-        a = $("#input").html();   //store first operand in 'a'
-        $("#input").html(btnVal);
-        $("#history").html(a);
-        
-        operatorVal = btnVal;
-        afterOp = true;
-        opScan = true;
+        a = a.append($("#input").html());
+        console.log("asdf");
+      }else{
+        a = $("#input").html();
+        console.log("qwerty");
       }
+      operatorVal = btnVal;
+      console.log(operatorVal)
+      console.log(a);
+      $("#input").html(operatorVal);
+      opScan = true;
+
     }
     //ac pushed --> clear input and history, reset operator
-    else if(btnVal === 'ac'){
+    else if (btnVal === 'ac') {
       console.log("e");
       $("#input").html(' ');
       $("#history").html(' ');
     }
     //ce pushed --> clear input, reset operator
-    else if(btnVal === 'ce'){
+    else if (btnVal === 'ce') {
       console.log("f");
       $("#input").html('  ');
-      
-    }
-    else if(afterOp === true){
-      console.log("g");
+
+    } else if (afterOp === true) {
       $("#history").append(operatorVal);
       $("#input").html(btnVal);
+      
       afterOp = false;
-      }    
-     else {
-       console.log("h");
-         $("#input").append(btnVal);
-      }
+    } else {
+      $("#input").append(btnVal);
+    }
   });
-  
+
 });
