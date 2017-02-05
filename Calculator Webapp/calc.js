00a22a13ca41c3272f375bd972afed6e606a5b4d
jswz72 <jacobdasword@gmail.com>
2017-02-04 20:08:12 -0500

    Cleanup calc.js
    Cleaned up code, removed redundant lines

--- Calculator Webapp/calc.js
+++ Calculator Webapp/calc.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
-  // console.log("ready");
   var afterOp = false;
-  //console.log("button success");
-  var expression; //collect button input
-  var a;
-  var operatorVal;
   var operatorArr = ['/', '*', '-', '+']
-  var opScan = false;
-  var b;
   var answer = false; //Flag: True if answer to calculation displayed on screen
 
   $("button").click(function() {
@@ -21,34 +14,24 @@ $(document).ready(function() {
       answer = false;
     }
 
-    //if equals button was pushed, move input--> history and calculate
+    //Calculate if '=' pushed
     if (btnVal === '=') {
       
       $("#input").html(math.eval($("#history").html()));
       answer = true;
-      opScan = false;
 
     }
-    //if operator pressed, make sure no other operator stored until cleared by '='
-    else if (btnVal === '/' || btnVal === '*' || btnVal === '-' || btnVal === '+') {
-      
-      operatorVal = btnVal;
-      $("#input").html(operatorVal);
-      $("#history").append(operatorVal);
-
-    }
-    //ac pushed --> clear input and history, reset operator
+    //ac pushed --> clear input and history
     else if (btnVal === 'ac') {
       $("#input").html('');
       $("#history").html('');
       
     }
-    //ce pushed --> clear input, reset operator
+    //ce pushed --> clear input & last operand
     else if (btnVal === 'ce') {
       $("#input").html('__');
       
       var historyArr = $("#history").html().split('');
-      //console.log(historyArr.length);
       for(var i = historyArr.length; i >= 0; i--){
         for(var j = 0; j < operatorArr.length; j++){
           if(historyArr[i] === operatorArr[j]){
@@ -61,16 +44,12 @@ $(document).ready(function() {
           i = -1;
         }
       }
-      //console.log(historyArr);
+      
       historyArr = historyArr.join('');
       console.log(historyArr);
       $("#history").html(historyArr);
       
-    } else if (afterOp === true) {
-      $("#history").append(operatorVal);
-      $("#input").html(btnVal);
-      
-      afterOp = false;
+      //All normal button presses
     } else {
       $("#input").html('');
       $("#input").html(btnVal);
